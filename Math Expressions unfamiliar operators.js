291459a116b4f1292a704abc77f068521cb177c3
Emanuel Joness <itzemanuelj@gmail.com>
2022-10-10 21:25:05 -0400

    mvp commit for math expressions unfimilar operator

---
+++ Math Expressions unfamiliar operators.js
@@ -0,0 +1,75 @@
+// Code a short form of x = x + 1; Use either of the two legal expressions.
+x++;
+
+// If x has a value of 100, what is the fastest way to reduce it to 99 with a math expression? Use either of the two legal expressions.
+100--;
+// x-- or --x
+
+var x = 50;
+var y = x++;
+// What is the value of y?          
+50
+// When the pluses come after the variable, its original value is assigned to the new variable before the incrementing variable is incremented. 
+
+var x = 50;
+var y = --x;
+// What is the value of y?
+49
+
+// code onto the screen to form a statement that assigns the original value of num to newNum and then increments num.
+var num = newNum++;
+
+// In a single statement, increment the variable num and assign its new value to a second variable, newNum, which hasn't been declared beforehand.
+var num = ++newNum;
+
+// In a single statement, decrement num and assign its original value to newNum, which hasn't been declared beforehand.
+var newNum = num--;
+
+// Using minimal code, decrement x. Use either of the two legal expressions.
+x--;
+
+// What is the long version of x++?
+x = x + 1;
+
+// In a single statement subtract 1 from a variable and assign the new value to another variable, which hasn't been declared beforehand. Both variables wind up with the same value.
+
+var cost = total--;
+
+// In a single statement add 1 to a variable and assign its original value to another variable, which hasn't been declared beforehand.
+var add = addition++;
+
+// Increment a variable. Use either of the two legal expressions. In a second statement, display its new value in an alert.
+var time = time++;
+alert(time);
+
+// Time goal: 15 seconds. (The timer starts when you click in the answer field.) Code the short form of x = x + 1;
+x++;
+
+// Time goal: 15 seconds. Using minimal code, decrement x. Use either of the two legal expressions.
+x--;
+
+// Time goal: 20 seconds. What is the long version of x++?
+x = x + 1;
+
+// Time goal: 25 seconds. In a single statement subtract 1 from a variable and assign the new value to another variable, which hasn't been declared beforehand. Both variables wind up with the same value.
+var travel = time--;
+
+// Time goal: 25 seconds. In a single statement add 1 to a variable and assign its original value to another variable, which hasn't been declared beforehand.
+var time = cost++;
+
+// Time goal: 25 seconds. Increment a variable, then display its new value in an alert. Use either of the two legal expressions.
+increase++;
+alert(increase);
+
+// Increment the variable using minimal code. Use either of the two legal expressions.
+// Display the new value in an alert.
+var total = 10;
+total++;
+alert(total);
+
+// Assign a number value to a variable.
+// Increment the variable using minimal code while assigning its incremented value to a second variable. Both variables wind up with the same value.
+// Display the value of the second variable in an alert.
+var cost = 100;
+newCost = cost++;
+alert(cost);
